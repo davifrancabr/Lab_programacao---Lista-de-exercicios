4b9006affdd274a73828e59a8a8f1001fc5ee0e9
davifranca <davicavalcantefranca@gmail.com>
2025-11-05 18:09:28 -0300

    Encontrando os números impares

---
+++ encontrando_os_numeros_impares.py
@@ -0,0 +1,14 @@
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3,
+           42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+
+
+def numeros_impares(lista: list[int]):
+    numeros_impar = []
+    for numero in lista:
+        if numero % 2 != 0:
+            numeros_impar.append(numero)
+    print(numeros_impar)
+    return numeros_impar
+
+
+numeros_impares(numeros)
