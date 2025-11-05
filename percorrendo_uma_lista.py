8edbccd41f09b071fc37506915a634a660b14c54
davifranca <davicavalcantefranca@gmail.com>
2025-11-05 15:10:07 -0300

    feat: atividade: percorrendo_uma_lista

---
+++ percorrendo_uma_lista.py
@@ -0,0 +1,10 @@
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3,
+           42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+
+
+def print_numeros(lista: list[int]):
+    for numero in lista:
+        print(numero)
+
+
+print_numeros(numeros)
